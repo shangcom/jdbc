3f801e2350c9b09fb7904f4994f5b7a8a57b9c34
shangcom <dndbs91@gmail.com>
2024-10-13 17:39:11 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리
    섹션 3 - 커넥션풀과 데이터소스 이해
    
    DataSource 예제2 - 커넥션 풀
    
    완료

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -67,8 +67,49 @@ public class MemberRepositoryV0 {
         } finally {
             close(rs, pstmt, con);
         }
+    }
 
+    public void update(String memberId, int money) throws SQLException {
 
+        String sql = "update member set money=? where member_id=?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize= {}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(null, pstmt, con);
+        }
+    }
+
+    public void delete(String meberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, meberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize= {}", resultSize);
+
+        } catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(null, pstmt, con);
+        }
     }
 
     /**
